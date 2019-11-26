06089de555901e3ffe1c9c708562bf0d15721629
Jean Lange <jean.f.lange@gmail.com>
2019-11-26 10:47:28 -0500

    working color blocks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ColorBlock from './ColorBlock';
 
 function App() {
+  let blocks = [];
+  for(let i = 0; i < 100; i++) {
+    blocks.push(<ColorBlock key={i} />);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {blocks}
     </div>
   );
 }
